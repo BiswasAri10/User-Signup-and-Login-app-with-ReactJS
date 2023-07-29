64b8c74f5d6802f9106d730cfcbcd06f85c84088
Arindam <bis.ips22@gmail.com>
2023-07-29 20:34:50 +0530

    Users redirected after login or password change

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import { useRef, useContext } from "react";
+import { useHistory } from 'react-router-dom';
 import AuthContext from "../../store/auth-context";
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
   const submitHandler = (event) => {
@@ -23,7 +25,7 @@ const ProfileForm = () => {
         },
       }
     ).then(response => {
-      
+      history.replace('/');
     });
   };
   return (
